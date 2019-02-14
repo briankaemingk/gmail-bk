717887e2de426a19751685e1b6192f4ccca146b4
Brian Kaemingk <brian.e.k@gmail.com>
2019-02-14 14:55:59 +0100

    Testing

--- app/__init__.py
+++ app/__init__.py
@@ -374,7 +374,7 @@ def watch():
 # Create scheduled job to run daily
 def initialize_cron_job():
     scheduler = BackgroundScheduler(timezone=utc)
-    scheduler.add_job(watch, 'cron', hour=13, minute=21)
+    scheduler.add_job(watch, 'cron', hour=13, minute=57)
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
 
