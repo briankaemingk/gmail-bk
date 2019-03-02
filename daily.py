fcbc7e73c3a1db1494ab49a93611bd2d4465628a
Brian Kaemingk <brian.e.k@gmail.com>
2019-03-02 14:17:56 +0100

    Testing scheduled job

--- daily.py
+++ daily.py
@@ -1,13 +1,39 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from app.models import User
+from app import create_user_creds
+import googleapiclient.discovery
+from app import db
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+API_SERVICE_NAME = 'gmail'
+API_VERSION = 'v1'
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=13, minute=11)
+@sched.scheduled_job('cron', hour=13, minute=19)
 def scheduled_job():
     users = User.query.all()
 
     for user in users:
-        print(user.email)
+
+        # Call the Gmail API, watch the label
+        print("******** in watch renewal *********")
+
+        users = User.query.all()
+
+        for user in users:
+            # Load credentials from the session.
+            credentials = create_user_creds(user)
+
+            gmail = googleapiclient.discovery.build(
+                API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+            watch_response = str(gmail.users().watch(userId='me', body=REQ).execute())
+            print("Watch renewed at: " + watch_response)
+
+            user.token = credentials.token
+            user.refresh_token = credentials.refresh_token
+            db.session.commit()
+
 
 sched.start()
